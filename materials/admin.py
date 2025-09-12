1d01d9fd5b7cf003033a1d8ae9dd433d401cdb40
fantasticlion <den20020805@gmail.com>
2025-09-12 04:27:07 +0300

    все эндпойнты протестированы через Postman - CRUD работает идеально!

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Course, Lesson
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    list_filter = ('title',)
+    search_fields = ('title', 'description')
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'video_link')
+    list_filter = ('course',)
+    search_fields = ('title', 'description')
+    raw_id_fields = ('course',)
